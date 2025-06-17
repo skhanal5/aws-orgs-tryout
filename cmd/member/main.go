b55cd838c50af2b1f4d0fb7ae9aac9f3e481d729
Subodh Khanal <skhanal5@terpmail.umd.edu>
2025-06-16 20:03:15 -0400

    Update main to retrieve the account name for a given account

--- cmd/member/main.go
+++ cmd/member/main.go
@@ -2,15 +2,16 @@ package main
 
 import (
 	"context"
-	"fmt"
+	"log"
 
 	"github.com/skhanal5/aws-orgs-tryout/internal/sdk/account"
 )
 
 func main() {
 	ctx := context.Background()
-	err := account.PutContactInformation(ctx, "804373361712")
+	name, err := account.GetAccountName(ctx, "804373361712")
 	if err != nil {
-		fmt.Printf("\nFailed to get account name: \n%s", err)
+		log.Fatalf("\nFailed to get account name: \n%s", err)
 	}
+	log.Printf("The name is: %s", name)
 }
